3894c166f5b2bad24d1fecbf784379a35db6918f
hwatanabe <hodaka.hw.watanabe@nts.ricoh.co.jp>
2015-09-27 19:00:44 +0900

    add ex03_01

---
+++ src/ch03/ex03_01/PassengerVehicle.java
@@ -0,0 +1,30 @@
+package ch03.ex03_01;
+
+public class PassengerVehicle extends Vehicle {
+	/** 座席数 */
+	private final int seatNum;
+	/** 座っている人数 */
+	private int passengerNum;
+
+	public PassengerVehicle(int seatNum) {
+		super();
+		this.seatNum = seatNum;
+	}
+
+	public PassengerVehicle(String owner, int seatNum) {
+		super(owner);
+		this.seatNum = seatNum;
+	}
+
+	public int getSeatNum() {
+		return seatNum;
+	}
+
+	public void setPassengerNum(int passengerNum) {
+		this.passengerNum = passengerNum;
+	}
+
+	public int getPassengerNum() {
+		return passengerNum;
+	}
+}
